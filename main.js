986e89a66c80f0b12aa1837920a5467b6acfc95c
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-08-06 12:20:34 +0530

    feat: add color params

--- main.js
+++ main.js
@@ -26,6 +26,8 @@ const params = {
   spin: 1,
   randomness: 0.2,
   randomnessPow: 3,
+  innerColor: "#ff6030",
+  outerColor: "#1b3984",
 }
 
 let geometry = null
@@ -41,7 +43,12 @@ function generateGalaxy() {
 
   geometry = new THREE.BufferGeometry()
 
+  const innerColor = new THREE.Color(params.innerColor)
+  const outerColor = new THREE.Color(params.outerColor)
+
   const positions = new Float32Array(params.count * 3)
+
+  const colors = new Float32Array(params.count * 3)
 
   for (let i = 0; i < params.count; i++) {
     const i3 = i * 3
@@ -50,6 +57,9 @@ function generateGalaxy() {
     const spinAngle = radius * params.spin
 
     const branchAngle = ((i % params.branches) / params.branches) * 2 * Math.PI
+
+    const mixedColor = innerColor.clone()
+    mixedColor.lerp(outerColor, radius / params.radius)
 
     const randomX =
       Math.pow(Math.random(), params.randomnessPow) *
@@ -64,17 +74,25 @@ function generateGalaxy() {
     positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX
     positions[i3 + 1] = 0 + randomY
     positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
+
+    colors[i3] = mixedColor.r
+    colors[i3 + 1] = mixedColor.g
+    colors[i3 + 2] = mixedColor.b
   }
 
   const bufferPosAttr = new THREE.BufferAttribute(positions, 3)
+  const bufferColorAttr = new THREE.BufferAttribute(colors, 3)
 
   geometry.setAttribute("position", bufferPosAttr)
+
+  geometry.setAttribute("color", bufferColorAttr)
 
   material = new THREE.PointsMaterial({
     size: params.particleSize,
     sizeAttenuation: params.sizeAttenuation,
     depthWrite: false,
     blending: THREE.AdditiveBlending,
+    vertexColors: true,
   })
 
   points = new THREE.Points(geometry, material)
@@ -105,6 +123,10 @@ debugGalaxy
 debugGalaxy
   .add(params, "randomnessPow", 1, 10, 0.001)
   .onFinishChange(generateGalaxy)
+
+debugGalaxy.addColor(params, "innerColor").onFinishChange(generateGalaxy)
+
+debugGalaxy.addColor(params, "outerColor").onFinishChange(generateGalaxy)
 
 /**
  * Sizes
